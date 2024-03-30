80b8e3e9be5f29a39aea662a572f54420b0d289f
Artur Arutyunyan <arturarutyunyan395@gmail.com>
2024-03-30 17:59:07 +0500

    Created project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        float secondsBetweenSpawn = 2;
+        StartCoroutine(DelayTime(secondsBetweenSpawn));
+    }
+
+    private IEnumerator DelayTime(float delay)
+    {
+        var wait = new WaitForSeconds(delay);
+        
+        while (enabled)
+        {
+            Spawn();
+            yield return wait;
+        }
+    }
+
+    private void Spawn()
+    {
+        int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+        Rigidbody2D copy = Instantiate(_enemyPrefab, _spawnPoints[spawnPointNumber].position, Quaternion.identity);
+    }
+}
